61ccab14bbb56e9ad37c9d257db3dedbb49e775b
pedro772 <plucena17230@gmail.com>
2023-04-08 19:35:31 -0300

    feat: add prop to navbar on homepage

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -3,51 +3,57 @@ import Link from "next/link";
 
 import styles from "./styles.module.scss";
 
-export function Navbar() {
+interface NavbarProps {
+  isOnHomePage?: boolean;
+}
+
+export function Navbar({ isOnHomePage }: NavbarProps) {
   return (
-    <nav className={styles.navbar}>
-      <Link href="/" className={styles.hamburger}>
-        <Image
-          src="/assets/shared/tablet/icon-hamburger.svg"
-          width={16}
-          height={15}
-          alt="Hamburger Icon"
-        />
-      </Link>
+    <nav className={isOnHomePage ? styles["navbar--homepage"] : styles.navbar}>
+      <div className={styles["navbar__options-container"]}>
+        <Link href="/" className={styles.hamburger}>
+          <Image
+            src="/assets/shared/tablet/icon-hamburger.svg"
+            width={16}
+            height={15}
+            alt="Hamburger Icon"
+          />
+        </Link>
 
-      <Link href="/">
-        <Image
-          src="/assets/shared/desktop/logo.svg"
-          width={140}
-          height={25}
-          alt="The Audiophile Logo, Audiophile written in white letters"
-        />
-      </Link>
+        <Link href="/">
+          <Image
+            src="/assets/shared/desktop/logo.svg"
+            width={140}
+            height={25}
+            alt="The Audiophile Logo, Audiophile written in white letters"
+          />
+        </Link>
 
-      <div className={styles.options}>
-        <Link href="/" className={styles.button__link}>
-          Home
-        </Link>
-        <Link href="/" className={styles.button__link}>
-          Headphones
-        </Link>
-        <Link href="/" className={styles.button__link}>
-          Speakers
-        </Link>
-        <Link href="/" className={styles.button__link}>
-          Earphones
+        <div className={styles.options}>
+          <Link href="/" className={styles.button__link}>
+            Home
+          </Link>
+          <Link href="/" className={styles.button__link}>
+            Headphones
+          </Link>
+          <Link href="/" className={styles.button__link}>
+            Speakers
+          </Link>
+          <Link href="/" className={styles.button__link}>
+            Earphones
+          </Link>
+        </div>
+
+        <Link href="/">
+          <Image
+            src="/assets/shared/desktop/icon-cart.svg"
+            width={23}
+            height={20}
+            alt="Cart Icon"
+            className={styles.cart}
+          />
         </Link>
       </div>
-
-      <Link href="/">
-        <Image
-          src="/assets/shared/desktop/icon-cart.svg"
-          width={23}
-          height={20}
-          alt="Cart Icon"
-          className={styles.cart}
-        />
-      </Link>
     </nav>
   );
 }
