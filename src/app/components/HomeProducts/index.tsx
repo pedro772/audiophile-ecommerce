5e2a2de0442661742756edc9ddbc5c059e0e21da
pedro772 <plucena17230@gmail.com>
2023-04-20 18:15:50 -0300

    feat: add last products to homepage

--- src/app/components/HomeProducts/index.tsx
+++ src/app/components/HomeProducts/index.tsx
@@ -2,19 +2,37 @@ import Link from "next/link";
 import MainButton from "../Buttons/MainButton";
 
 import styles from "./styles.module.scss";
+import zx9Styles from "./zx9.module.scss";
+import zx7Styles from "./zx7.module.scss";
+import yx1Styles from "./yx1.module.scss";
 
 export function HomeProducts() {
   return (
-    <div style={{ marginTop: "120px" }}>
-      <div className={styles.zx9}>
-        <div className={styles.zx9__image} />
-        <div className={styles.zx9__background} />
-        <div className={styles.zx9__info}>
-          <h2 className={styles.zx9__header}>ZX9 SPEAKER</h2>
-          <p className={styles.zx9__description}>
+    <div className={styles.container}>
+      <div className={zx9Styles.zx9}>
+        <div className={zx9Styles.zx9__image} />
+        <div className={zx9Styles.zx9__background} />
+        <div className={zx9Styles.zx9__info}>
+          <h2 className={zx9Styles.zx9__header}>ZX9 SPEAKER</h2>
+          <p className={zx9Styles.zx9__description}>
             Upgrade to premium speakers that are phenomenally built to deliver
             truly remarkable sound.
           </p>
+          <MainButton link="/" name="See product" type="secondary" />
+        </div>
+      </div>
+
+      <div className={zx7Styles.zx7}>
+        <div className={zx7Styles.zx7__info}>
+          <h2 className={zx7Styles.zx7__header}>ZX7 SPEAKER</h2>
+          <MainButton link="/" name="See product" type="secondary" />
+        </div>
+      </div>
+
+      <div className={yx1Styles.yx1}>
+        <div className={yx1Styles.yx1__image} />
+        <div className={yx1Styles.yx1__info}>
+          <h2 className={yx1Styles.yx1__header}>YX1 EARPHONES</h2>
           <MainButton link="/" name="See product" type="secondary" />
         </div>
       </div>
