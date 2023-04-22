c91dd9c6f8c1b50e142c3f9db6df36cdb5b570ea
pedro772 <plucena17230@gmail.com>
2023-04-21 22:36:51 -0300

    feat: add earphones page

--- src/app/(category)/earphones/page.tsx
+++ src/app/(category)/earphones/page.tsx
@@ -1,5 +1,6 @@
 import { getData } from "@/app/api/data/getData";
 import { CategoryHeader } from "@/app/components/CategoryHeader";
+import { CategoryProduct } from "@/app/components/CategoryProduct";
 
 async function Earphones() {
   const data = await getData();
@@ -7,6 +8,18 @@ async function Earphones() {
   return (
     <>
       <CategoryHeader title="EARPHONES" />
+      {data.map((product) =>
+        product.category === "earphones" ? (
+          <CategoryProduct
+            key={product.id}
+            imagePath={product.categoryImage.mobile}
+            isNewProduct={product.new}
+            name={product.name}
+            description={product.description}
+            slug={product.slug}
+          />
+        ) : null
+      )}
     </>
   );
 }
