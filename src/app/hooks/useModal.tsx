7dc2376a298b1bb42eb85036264aec134ebaa456
pedro772 <plucena17230@gmail.com>
2023-04-26 22:10:21 -0300

    fix: modal exhibition

--- src/app/hooks/useModal.tsx
+++ src/app/hooks/useModal.tsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 
 export function useModal() {
   const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState();
+  const [modalContent, setModalContent] = useState<any>();
 
   const handleModal = (content: any = false) => {
     if (content) {
-      setShowModal((prevShowModal) => !prevShowModal);
+      if (
+        content.type === (modalContent && modalContent.type) ||
+        modalContent === undefined ||
+        !showModal
+      ) {
+        setShowModal((prevShowModal) => !prevShowModal);
+      }
+
       setModalContent(content);
     } else {
       setShowModal(false);
