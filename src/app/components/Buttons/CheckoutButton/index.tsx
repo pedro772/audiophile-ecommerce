f9d3b0b429640c560ada37e3ae95bd02ec4a2451
pedro772 <plucena17230@gmail.com>
2023-04-29 17:09:05 -0300

    feat: add form support to button checkout

--- src/app/components/Buttons/CheckoutButton/index.tsx
+++ src/app/components/Buttons/CheckoutButton/index.tsx
@@ -4,20 +4,24 @@ import styles from "../styles.module.scss";
 
 interface CheckoutButtonProps {
   link?: string;
+  form?: string;
   name: string;
   handleClick?: (_?: any) => void;
 }
 
 export default function CheckoutButton({
   link,
+  form,
   name,
   handleClick,
 }: CheckoutButtonProps) {
   return link ? (
-    <Link href={link}>
+    <Link href={link} style={{ width: "100%" }}>
       <button
         className={styles["button--checkout"]}
         onClick={handleClick && handleClick}
+        type={form ? "submit" : "button"}
+        form={form}
       >
         {name}
       </button>
@@ -26,6 +30,8 @@ export default function CheckoutButton({
     <button
       className={styles["button--checkout"]}
       onClick={handleClick && handleClick}
+      type={form ? "submit" : "button"}
+      form={form}
     >
       {name}
     </button>
