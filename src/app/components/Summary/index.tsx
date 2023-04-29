68214526c163ec63dc9de04f67ce970a850de6af
pedro772 <plucena17230@gmail.com>
2023-04-29 17:16:01 -0300

    feat: add summary component

---
+++ src/app/components/Summary/index.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Image from "next/image";
+
+import { useCartContext } from "@/app/context/cart";
+import { getCartImagePath, getCartProductName } from "@/utils/getCartData";
+
+import styles from "./styles.module.scss";
+import CheckoutButton from "../Buttons/CheckoutButton";
+
+export function Summary() {
+  const { itemsInCart } = useCartContext();
+  let total = 0;
+  itemsInCart.forEach((item) => {
+    total += item.product.price * item.quantity;
+  });
+
+  return (
+    <>
+      <div className={styles.summary__products}>
+        {itemsInCart.map((item) => (
+          <div key={item.product.id} className={styles.summary__product}>
+            <Image
+              src={getCartImagePath(item.product)}
+              alt=""
+              width={64}
+              height={64}
+            />
+            <div className={styles.product__info}>
+              <p className={styles.product__name}>
+                {getCartProductName(item.product)}
+              </p>
+              <p
+                className={styles.product__price}
+              >{`$ ${item.product.price}`}</p>
+            </div>
+            <span className={styles.product__quantity}>x{item.quantity}</span>
+          </div>
+        ))}
+      </div>
+      <div className={styles.prices}>
+        <div className={styles.prices__detailed}>
+          <div className={styles.prices__container}>
+            <p className={styles.prices__expense}>TOTAL</p>
+            <h6 className={styles.prices__price}>{`$ ${Math.round(
+              total * 1.2
+            )}`}</h6>
+          </div>
+          <div className={styles.prices__container}>
+            <p className={styles.prices__expense}>SHIPPING</p>
+            <h6 className={styles.prices__price}>$ 50</h6>
+          </div>
+          <div className={styles.prices__container}>
+            <p className={styles.prices__expense}>{`VAT (INCLUDED)`}</p>
+            <h6 className={styles.prices__price}>{`$ ${Math.round(
+              total * 0.2
+            )}`}</h6>
+          </div>
+        </div>
+
+        <div className={styles.prices__container}>
+          <p className={styles.prices__expense}>GRAND TOTAL</p>
+          <h6 className={styles["prices__price--total"]}>{`$ ${Math.round(
+            total * 1.2 + 50
+          )}`}</h6>
+        </div>
+      </div>
+      <CheckoutButton name="Continue" form="checkout" />
+    </>
+  );
+}
